02be9561750d5c4f4d310c0d468228e2b813d0d8
pabloeduardogimenez <pabloedug7@gmail.com>
2025-03-11 20:42:42 -0300

    first commit

---
+++ aula1.js
@@ -0,0 +1,33 @@
+var nome = "Pablo";
+var idade = 10; // number -> int
+var peso = 23.4 ; // number ->float 
+var andando = false; // boolean
+
+const cidade = "curitiba";
+//variavel 
+let fruta = "abacaxi";
+
+
+// == igual
+// != diferente
+// > maior 
+// < menor 
+// >= maior ou igual
+// >= menor ou igual
+// === exatamente igual
+// !== exatamente diferente
+
+
+if (idade >= 65){
+    console.log("idoso");
+} else if (idade >= 18){
+    console.log("adulto");
+}else if (idade >=12) {
+    console.log("adolescente");
+} else  {
+    console.log("criança");
+};
+
+if (cidade = "curitiba" ){
+    console.log("verdadeiro");
+};
