9ac991084e90c4e41130d9d15e4821f984cd50af
pabloeduardogimenez <pabloedug7@gmail.com>
2025-03-11 21:37:08 -0300

    add arquivos novos

---
+++ exercicio.js
@@ -0,0 +1,24 @@
+let M = "bom dia" ;
+let V = "boa tarde";
+let N = "boa noite";
+
+function letra (n1){
+    if (n1 == "M"){
+        console.log(M);
+    } else if (n1 == "V"){
+        console.log(V);
+    }
+    else if (n1 == "N"){
+        console.log(N);
+    }
+}
+
+letra("M");
+letra("V");
+letra("N");
+
+function raízes ( a, b , c){
+    if ( a == 0 ){
+        
+    }
+}
