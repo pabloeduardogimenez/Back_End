b979a82b06c37f08ecbc43899a7dbedeeb275d57
pabloeduardogimenez <pabloedug7@gmail.com>
2025-03-14 21:43:57 -0300

    auladia 14/03/25

--- exercicio3.js
+++ exercicio3.js
@@ -1,6 +1,46 @@
-// calculadora  caixa eletrênicar
+console.log("Qual o valor de saque ?");
 
-console.log("Valor de saque");
-// valor minimo é 10 e valor maximo R$ 600
+process.stdin.on('data', function(valor){
+    if (valor >= 601 || valor <= 10){
+        console.log("Valor de saque não poder ser menor R$ 10,00 ou maior  R$ 600,00.");
+        process.exit(); 
+        }   
 
-// dividir o 
+    let cem = 0;
+    let cinq = 0;
+    let dez = 0;
+    let cinco = 0;
+    let um = 0;
+    let resto = 0;        
+    cem = parseInt(valor / 100);
+    resto = valor % 100;
+
+
+    if (resto > 0 )
+    {
+        valor = resto;
+        cinq = parseInt(valor / 50);
+        resto = valor % 50;
+        
+        if (resto > 10){
+            dez = parseInt(valor/ 10);
+            resto = valor % 10;
+
+        }
+        if (resto > 5){
+            valor =  resto - valor;
+            cinco = parseInt(valor / 5);
+            resto = valor % 5;
+        }
+        if ( resto >= 1){
+            um = parseInt(valor / 1);
+        }     
+        
+    }
+    console.log(cem+" notas de 100 " );
+    console.log(cinq +" notas de 50 " );
+    console.log(dez +" notas de 10 ");
+    console.log(cinco +" notas de 5 ");
+    console.log(um + "notas de 1");
+    
+});
--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 // servidor 
 
 const express = require("express");
-
 const app = express();
+const fs = require("fs");
+const { loadEnvFile } = require("process");
 
-app.get("/", function(req , res){
-    res.status(404).send("oi turma");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/contato", function(req , res){      
+    let valores = req.query;
+    res.json(valores);
+});
+
+app.get("/contato", function(req, rec){
+    fs.readFile("alunos.cvs", utf8Encode,function(erro, dados){
+        let linha = dados.split("\n");
+        let html = "<ul>";
+        linha.forEach(function(item){
+            html = html +  "<li>" + item + "</li>"               
+        });
+        html += "</lu>";
+        res.send(html);
+    });
+    
+
+});
+app.post("/calc", function(req, res){
+    let resultado = 0 ;
+    resultado = parseInt(req.body.num1) + parseInt(req.body.num2);
+    res.send("resultado = " + resultado);
+});
+
+app.post("/contato", function(req, res){   
+    let val = req.body.nome + "," + req.body.email + "\n";
+
+    let options  = {
+        flag: "a"
+    };
+    fs.writeFile("alunos.cvs", val, options,function () {
+        res.send("email cadastrado");
+    })
+    
+});
+
+app.get("/", function(req, res){
+    res.send("Enviar formulario");
 });
 
 
